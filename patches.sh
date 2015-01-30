f8b3a52970f1dc02042dd23772867173cac4931f
Arslan Kirmani <arslankirmani9600@gmail.com>
2015-01-30 03:12:59 -0800

    cleanup

--- patches.sh
+++ patches.sh
@@ -16,12 +16,6 @@ fi
 # frameworks/av : Add back missing msm7x27a to fix compilation
 cherries+=(79582)
 
-# libstagefright: Make it possible to skip OMX buffer reallocation
-cherries+=(79137)
-
-# libstagefright: Make it possible to skip OMX buffer reallocation (2)
-cherries+=(84730)
-
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
