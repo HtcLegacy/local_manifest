8e1d1b92c4b67a20cc68d1dab1fdab03b7297f98
Arslan Kirmani <arslankirmani9600@gmail.com>
2015-01-30 03:09:29 -0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -16,20 +16,11 @@ fi
 # frameworks/av : Add back missing msm7x27a to fix compilation
 cherries+=(79582)
 
-# Build : Add back support for msm7x27a Board
-#cherries+=(79581)
+# libstagefright: Make it possible to skip OMX buffer reallocation
+cherries+=(79137)
 
-# build: Add option to disable block-based ota
-cherries+=(78849)
-
-# mtdutils: Fix mounting partitions by-name
-(cherries+=(83404/1)
-
-# install: disable signature checking on eng and userdebug builds
-cherries+=(81797)
-
-# lowmemorykiller: Overwrite LMK parameters for low-tier devices
-cherries+=(86327)
+# libstagefright: Make it possible to skip OMX buffer reallocation (2)
+cherries+=(84730)
 
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
